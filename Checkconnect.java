9715569f9d971e9679a8c82ed2c6bf40c1b5f187
Huyen Mai <maithihuyen216@gmail.com>
2024-03-06 22:56:47 +0700

    change code

--- Checkconnect.java
+++ Checkconnect.java
@@ -1,7 +1,7 @@
 public class Checkconnect {
     public static void main(String[] args) {
         String a="Hello";
-        int b=10;
+        int b=10, c=5;
         System.out.println(a);
         //Add comment
 
